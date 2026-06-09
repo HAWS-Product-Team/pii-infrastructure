e27faaf982e670ed07500a850c6fae3010feda29
Oxfordgreen95 <lukep.creative@gmail.com>
2026-06-09 16:03:47 -0500

    Destroyed NAT gateway, Elastic IP, and associated route to save costs.

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -119,23 +119,6 @@ resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_eip" "nat" {
-  domain = "vpc"
-}
-
-
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_a.id
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
-resource "aws_route" "private_nat" {
-  route_table_id         = aws_route_table.private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.main.id
-}
 
 resource "aws_route_table_association" "private_a" {
   subnet_id      = aws_subnet.private_a.id
@@ -170,7 +153,7 @@ module "datapipeline" {
   depends_on = [
   aws_route_table_association.private_a,
   aws_route_table_association.private_b,
-  aws_route.private_nat
+
 ]
 }
 
